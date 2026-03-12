d456717e8ad1724ae21b4525481ac679182d216e
gaelramos-ship-i <gael.ramos@laplateforme.io>
2026-03-12 14:01:35 +0100

    maj competences

--- src/pages/Competences.tsx
+++ src/pages/Competences.tsx
@@ -7,8 +7,8 @@ const  Competences = () => {
         <h2>Mes <span>compétences</span></h2>
         <div>
             {data.map((competence) => (
-                <article key={competence.id}>
-                    <img src={competence.image} alt={competence.title} />;
+                <article className='artBack' key={competence.id}>
+                    <img src={competence.image} alt={competence.title} />
                     <h3>{competence.title}</h3>
                 </article>
             ))}
